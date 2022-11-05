fbe31a7ea638c9048b88c10e1dfd435b9710e3ef
Jotaherrera <juanherreramen13@gmail.com>
2022-11-05 13:16:44 -0500

    Add base project

---
+++ CSVData/frmCSVDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSVData
+{
+    public partial class CSVDB : Form
+    {
+        public CSVDB()
+        {
+            InitializeComponent();
+        }
+    }
+}
